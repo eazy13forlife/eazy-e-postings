c82806aa5ebe793fe7593c8a4147c0567d38455a
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-07-05 02:36:11 -0700

    uninstalled package.json and ran npm install

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //configure express to use our static path,which is the production build path
-const productionBuildPath = path.join(__dirname, "./build/");
+const productionBuildPath = path.join(__dirname, "./build");
 app.use(express.static(productionBuildPath));
 
 //wildcard for anything that can't be found in the production build path
