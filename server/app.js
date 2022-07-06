fbc0eb30c458fefcc2a19c4294af00bbd1abe7a4
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-07-05 21:08:45 -0700

    Revert to 5abf8a62a6f6af54ecd742d025e4b1150385a325

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+//create express application
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//configure express to use our static path,which is the production build path
+const productionBuildPath = path.join(__dirname, "./build");
+app.use(express.static(productionBuildPath));
+
+//wildcard for anything that can't be found in the production build path
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Eazy-E Postings is running on port ${port}`);
+});
