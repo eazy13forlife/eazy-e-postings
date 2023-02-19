042b3dd5f297a8f6821213958306887d00c40dee
Eric B <eric_baffour-addo@alumni.brown.edu>
2023-02-19 09:30:48 -0800

    Get user location on initial render of EntryPage

--- src/views/EntryPage/index.js
+++ src/views/EntryPage/index.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import { fetchUserLocation } from "../../actions";
 import JobSearchForm from "../../components/JobSearchForm";
 import "./index.scss";
 
 const EntryPage = () => {
+  const dispatch = useDispatch();
+
+  const userLocation = useSelector((state) => {
+    return state.userLocation;
+  });
+
+  //on initial render, if no user location, set one
+  useEffect(() => {
+    const getInitialValues = async () => {
+      await dispatch(fetchUserLocation());
+    };
+
+    if (!userLocation) {
+      getInitialValues();
+    }
+  }, []);
+
   return (
     <div className="EntryPage">
       <div className="container">
